27a73fb0dae7ac75547ac6a11567fa19355dd217
Mathieu Sandana <sandana@iMac-de-Mathieu.local>
2019-05-15 00:36:58 +0200

    GNL basic

--- get_next_line/get_next_line_xcode.c
+++ get_next_line/get_next_line_xcode.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#define BUFF_SIZE 100
+#define BUFF_SIZE 10
 
 int	get_next_line (int fd, char **line)
 {
@@ -10,23 +10,18 @@ int	get_next_line (int fd, char **line)
 	size = BUFF_SIZE;
 	int c;
 	c = 0;
-
 	int err;
 	err = 0;
-
 	int i;
 	i = 0;
-
 	int ret;
 	char *buf;
+	int j;
+	j = 0;	
 
 	if(!(buf = (char*)malloc(sizeof(*buf) * (BUFF_SIZE + 1))))
 		return (0);
 
-
-	int j;
-	j = 0;
-	
 	while (j < size)
 	{
 		buf[j] = '\0';
@@ -38,50 +33,44 @@ int	get_next_line (int fd, char **line)
 	printf("buf avant fd et ret: %s\n", buf);
 
 
-
-	write(1,"rentre2\n", 8);
 	fd = open(path, O_RDONLY);
 	if (fd == -1)
 		err = -1;
 
 
-	    write(1,"rentre3\n", 8);
 	ret = read(fd, buf, size);
 	
-	    write(1,"rentre4\n", 8);
 	buf[ret] = '\0';
-    write(1,"rentre5\n", 8);	
-	
-	while (i <= size)
-	{
-		if (buf[i] == '\n')
-		{
-			c++;
-			break;
-		}
-		i++;
-	}
 	*line = buf;
-write(1,"rentre6\n", 8);
 
 	printf("line avant trim: %s \n", *line);
-	//trim line
-	int k;
-	k = 0;
-
-	while (line[0][k] != '\n' && k <= BUFF_SIZE)
-		k++;
-	if (line[0][k] == '\n')
-	{	
-		while (line[0][k] != '\0' && k <= BUFF_SIZE)	
-		{
-			line[0][k] = '\0';
-			k++;
-		}
 	
-	}		;
+	//trim line
+		int k  = 0;
+		int x = 1;
+		int xsize = BUFF_SIZE * x;
+
+		while (line[0][k] != '\n' && k <= BUFF_SIZE)
+			k++;
+
+		if (line[0][k] != '\n')
+		{
+			x++;
+			 if(!(buf = (char*)malloc(sizeof(*buf) * (xsize + 1))))
+			ret = read(fd, buf, xsize);
+			*(line) = *(buf);
+		}
+		if (line[0][k] == '\n')
+		{
+			while (line[0][k] != '\0' && k <= BUFF_SIZE)
+			{
+				line[0][k] = '\0';
+				k++;
+			}
+		}
+	}
 	printf("line dans gnl: %s \n", *line);
-	printf("line[0][1] dans gnl: %c \n", line[0][1]);	
+	printf("line[0][1] dans gnl: %c \n", line[0][1]);
 	printf("fd dans gnl: %d \n", fd);
 	printf("ret: %d \n", ret);
 	printf("i: %d \n", i);
@@ -104,7 +93,6 @@ int main()
 	char *line[BUFF_SIZE];
 //	line = NULL;
 	printf("fd dans le main: %d \n", fd);
-//	write(1,"rentre1: \n", 8);
 	get_next_line(fd, line);
 	printf("line apre gnl: %s \n", *line);
 	printf("resultat du return gnl: %d \n", get_next_line(fd, line));
